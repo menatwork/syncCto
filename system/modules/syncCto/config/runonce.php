b16f0747ce4eb9e19bf4a9c10800413a8addf54c
Stefan heimes <stefan_heimes@hotmail.com>
2012-12-07 11:20:11 +0100

    Add a function for creating the default syncCto folders for backup files.

--- system/modules/syncCto/config/runonce.php
+++ system/modules/syncCto/config/runonce.php
@@ -64,6 +64,8 @@ class SyncCtoErClient extends RepositoryBackendModule
 
     public function run()
     {
+		$this->checkFolders();
+		
         if ($this->blnNoError == false)
         {
             return;
@@ -80,6 +82,42 @@ class SyncCtoErClient extends RepositoryBackendModule
             $_SESSION['TL_ERROR'][] = $exc->getMessage();
         }
     }
+	
+	protected function checkFolders()
+	{
+		// Get folders from config
+		$strBackupDB	 = $this->standardizePath($GLOBALS['SYC_PATH']['db']);
+		$strBackupFile	 = $this->standardizePath($GLOBALS['SYC_PATH']['file']);
+		$strTemp		 = $this->standardizePath($GLOBALS['SYC_PATH']['tmp']);
+
+		$objHt	 = new File('system/modules/syncCto/config/.htaccess');
+		$strHT	 = $objHt->getContent();
+		$objHt->close();
+
+		// Check each one 
+		if (!file_exists(TL_ROOT . '/' . $strBackupDB))
+		{
+			new Folder($strBackupDB);
+
+			$objFile = new File($strBackupDB . '/' . '.htaccess');
+			$objFile->write($strHT);
+			$objFile->close();
+		}
+
+		if (!file_exists(TL_ROOT . '/' . $strBackupFile))
+		{
+			new Folder($strBackupFile);
+
+			$objFile = new File($strBackupFile . '/' . '.htaccess');
+			$objFile->write($strHT);
+			$objFile->close();
+		}
+
+		if (!file_exists(TL_ROOT . '/' . $strTemp))
+		{
+			new Folder($strTemp);
+		}
+	}
 
     protected function writeXML()
     {
@@ -250,6 +288,44 @@ class SyncCtoErClient extends RepositoryBackendModule
 
         return $arrSort;
     }
+	
+	// Helper
+
+	/**
+	 * Standardize path for folder
+	 * No TL_ROOT, No starting /
+	 * 
+	 * @return string the normalized path
+	 */
+	public function standardizePath()
+	{
+		$arrPath = func_get_args();
+
+		if (count($arrPath) == 0 || $arrPath == null || $arrPath == "")
+		{
+			return "";
+		}
+
+		$strVar = "";
+
+		foreach ($arrPath as $itPath)
+		{
+			$itPath = str_replace(array(TL_ROOT, "\\"), array("", "/"), $itPath);
+			$itPath = explode("/", $itPath);
+
+			foreach ($itPath as $itFolder)
+			{
+				if ($itFolder == "" || $itFolder == "." || $itFolder == "..")
+				{
+					continue;
+				}
+
+				$strVar .= "/" . $itFolder;
+			}
+		}
+
+		return preg_replace("/^\//i", "", $strVar);
+	}
 
 }
 
