83201cca8af7ae87d7d079f53179ff58a09b72c2
Stefan heimes <stefan_heimes@hotmail.com>
2013-01-29 17:04:48 +0100

    Add new human readable table names

--- system/modules/syncCto/languages/de/tl_syncCto_database.php
+++ system/modules/syncCto/languages/de/tl_syncCto_database.php
@@ -27,8 +27,14 @@
  * @filesource
  */
 
-$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_calendar_events']            = 'Event Einträge';
-$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_form_field']                 = 'Formular Felder';
-$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_member_group']               = 'Mitglieder Gruppe';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_faq_category']             = 'FAQ Kategorien';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_style']                    = 'Stylesheet';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_calendar_events']          = 'Eventdetails';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_form_field']               = 'Formularfelder';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_news_archive']             = 'Nachrichtenarchiv';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_newsletter_channel']       = 'Newsletterverteiler';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_newsletter_recipients']    = 'Newsletterabonnenten';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_repository_instfiles']     = 'Erweiterungsverwaltung (Dateien)';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_task_status']              = 'Task-Center Status';
 
 ?>
