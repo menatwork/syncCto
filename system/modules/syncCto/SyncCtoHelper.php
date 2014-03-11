6fc314fd58b895811e9b32b353e8bc8171edebb5
Stefan Heimes <stefan_heimes@hotmail.com>
2014-03-11 18:34:47 +0100

    Add a new function "parseSize" to parse values from the php ini.

--- system/modules/syncCto/SyncCtoHelper.php
+++ system/modules/syncCto/SyncCtoHelper.php
@@ -84,6 +84,35 @@ class SyncCtoHelper extends Backend
 
         return self::$instance;
     }
+
+    /* -------------------------------------------------------------------------
+     * Helper
+     */
+
+    /**
+     * Parse size
+     * @see http://us2.php.net/manual/en/function.ini-get.php#example-501
+     *
+     * @param string $size
+     *
+     * @return int|string
+     */
+    static public function parseSize($size)
+    {
+        $size = trim($size);
+        $last = strtolower($size[strlen($size)-1]);
+        switch($last) {
+            // The 'G' modifier is available since PHP 5.1.0
+            case 'g':
+                $size *= 1024;
+            case 'm':
+                $size *= 1024;
+            case 'k':
+                $size *= 1024;
+        }
+
+        return $size;
+    }
     
     /* -------------------------------------------------------------------------
      * Contao 2.xx and 3.xx helper
