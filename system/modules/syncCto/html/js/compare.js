ea4a644f30fe4ebb4a9a155d3d1fd4655bcad8e1
Stefan Heimes <stefan_heimes@hotmail.com>
2014-03-13 19:07:55 +0100

    Update th js for the new version

--- system/modules/syncCto/html/js/compare.js
+++ system/modules/syncCto/html/js/compare.js
@@ -1,2 +1,72 @@
 /* Copyright (c) MEN AT WORK 2012 :: LGPL license */
-window.addEvent("domready",function(){$$("input[name=delete]").set("disabled",true);$each($$(".checkbox input[type=checkbox]"),function(c){c.addEvents({click:function(){var d=($$(".checkbox input[type=checkbox]:checked").length!=0)?true:false;if(d==true){$$("input[name=delete]").set("disabled",false);$$("input[name=transfer]").set("disabled",true)}else{$$("input[name=delete]").set("disabled",true);$$("input[name=transfer]").set("disabled",false)}}})});if(window.HtmlTable){$$("body").addClass("table-sort");HtmlTable.defineParsers({dimension:{match:".*(Bytes|kB|mB|gB)",convert:function(){var d=this.get("text").replace(/.*(\d|,| )/,"").toString().toLowerCase();var c=this.get("text").replace(",",".").toFloat();if(d=="kb"){c=c*1024}else{if(d=="mb"){c=c*[Math.pow(1024,2)]}else{if(d=="gb"){c=c*[Math.pow(1024,3)]}}}return c},number:true}});var b=new HtmlTable($("normalfilelist"),{sortIndex:3,parsers:["string","dimension","string"],sortable:true}).enableSort({sortable:true,sortIndex:3});var a=new HtmlTable($("bigfilelist"),{sortIndex:3,parsers:["string","dimension","string"],sortable:true}).enableSort({sortable:true,sortIndex:3})}});
+
+window.addEvent("domready",function(){
+    
+    $$('input[name=delete]').set('disabled', true);
+
+	$$('.checkbox input[type=checkbox]').each(function(item){
+        item.addEvents({
+            click: function(){
+                var blnFound = ($$('.checkbox input[type=checkbox]:checked').length != 0) ? true : false;
+                
+                if (blnFound == true){
+                    $$('input[name=delete]').set('disabled', false);
+                    $$('input[name=transfer]').set('disabled', true);
+                } else {
+                    $$('input[name=delete]').set('disabled', true);
+                    $$('input[name=transfer]').set('disabled', false);
+                }
+            }
+        });    
+    });
+
+    if(window.HtmlTable)
+    {
+        $$('body').addClass('table-sort');
+        
+        HtmlTable.defineParsers({
+            dimension: {
+                match: '.*(Bytes|kB|mB|gB)',
+                convert: function(){
+                    var strDimension = this.get('text').replace(/.*(\d|,| )/, '').toString().toLowerCase();
+                    var floatVal = this.get('text').replace(',', '.').toFloat();
+
+                    if(strDimension == "kb")
+                    {
+                        floatVal = floatVal * 1024;
+                    }
+                    else if(strDimension == "mb")
+                    {
+                        floatVal = floatVal*[Math.pow(1024,2)];
+                    }
+                    else if(strDimension == "gb")
+                    {
+                        floatVal = floatVal*[Math.pow(1024,3)];
+                    }
+
+                    return floatVal;
+                },
+                number: true
+            }
+        });    
+
+        var myHtmlTableNormal = new HtmlTable($('normalfilelist'), {
+            sortIndex: 3,
+            parsers: ['string', 'dimension', 'string'],
+            sortable: true
+        }).enableSort({
+            sortable: true, 
+            sortIndex: 3
+        });
+        
+        var myHtmlTableBig = new HtmlTable($('bigfilelist'), {
+            sortIndex: 3,
+            parsers: ['string', 'dimension', 'string'],
+            sortable: true
+        }).enableSort({
+            sortable: true, 
+            sortIndex: 3
+        });
+    }
+
+});
