b9c1327ca7b6c88984197f259e91d41c3f15c638
Stefan Heimes <stefan_heimes@hotmail.com>
2014-04-02 17:59:32 +0200

    Update the runonce to remove the old syncCto settings from the localconfig.

--- system/modules/syncCto/config/runonce.php
+++ system/modules/syncCto/config/runonce.php
@@ -1,11 +1,11 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
  *
  * @copyright  MEN AT WORK 2014
  * @package    syncCto
- * @license    GNU/LGPL 
+ * @license    GNU/LGPL
  * @filesource
  */
 
@@ -14,9 +14,48 @@
  */
 class SyncCtoRunOnce
 {
+    /**
+     * The SyncCto Helper.
+     *
+     * @var SyncCtoHelper
+     */
+    protected $objSyncCtoHelper;
+
+    /**
+     * Old settings to be replaced with the new ones.
+     *
+     * @var array
+     */
+    protected $arrFolderBlacklist = array
+    (
+        'assets/css',
+        'assets/images',
+        'assets/js',
+        'composer/cache',
+        'system/cache',
+        'system/backup',
+        'system/html',
+        'system/logs',
+        'system/scripts',
+        'system/tmp',
+        '*/syncCto_backups',
+        'files/translations/export',
+        'files/translations/import'
+    );
+
+    /**
+     * Init
+     */
+    public function __construct()
+    {
+        $this->objSyncCtoHelper = SyncCtoHelper::getInstance();
+    }
 
     public function run()
     {
+        // Check config
+        $this->updateConfig();
+
         // Check the folders
         $this->checkFolders();
 
@@ -31,15 +70,56 @@ class SyncCtoRunOnce
         }
     }
 
+    protected function updateConfig()
+    {
+        $arrClearConfig = array();
+
+        // Cleanup the folder blacklist.
+        $strLocalconfig = $GLOBALS['TL_CONFIG']['syncCto_folder_blacklist'];
+        $strLocalconfig = $this->cleanEntries($strLocalconfig, 'folder_blacklist', $this->arrFolderBlacklist);
+
+        $arrClearConfig['syncCto_folder_blacklist'] = $strLocalconfig;
+
+        // Cleanup the file blacklist.
+        $strLocalconfig = $GLOBALS['TL_CONFIG']['syncCto_file_blacklist'];
+        $strLocalconfig = $this->cleanEntries($strLocalconfig, 'file_blacklist', array());
+
+        $arrClearConfig['syncCto_file_blacklist'] = $strLocalconfig;
+
+        // Cleanup the folder whitelist.
+        $strLocalconfig = $GLOBALS['TL_CONFIG']['syncCto_folder_whitelist'];
+        $strLocalconfig = $this->cleanEntries($strLocalconfig, 'folder_whitelist', array());
+
+        $arrClearConfig['syncCto_folder_whitelist'] = $strLocalconfig;
+
+        // Cleanup the file localconfig blacklist.
+        $strLocalconfig = $GLOBALS['TL_CONFIG']['syncCto_local_blacklist'];
+        $strLocalconfig = $this->cleanEntries($strLocalconfig, 'local_blacklist', array());
+
+        $arrClearConfig['syncCto_local_blacklist'] = $strLocalconfig;
+
+        // Cleanup the file localconfig blacklist.
+        $strLocalconfig = $GLOBALS['TL_CONFIG']['syncCto_hidden_tables'];
+        $strLocalconfig = $this->cleanEntries($strLocalconfig, 'table_hidden', array());
+
+        $arrClearConfig['syncCto_hidden_tables'] = $strLocalconfig;
+
+        // Save back to the localconfig.
+        $this->objSyncCtoHelper->importConfig($arrClearConfig);
+    }
+
+    /**
+     * Create the folders and protect them.
+     */
     protected function checkFolders()
     {
         // Get folders from config
-        $strBackupDB     = $this->standardizePath($GLOBALS['SYC_PATH']['db']);
-        $strBackupFile     = $this->standardizePath($GLOBALS['SYC_PATH']['file']);
-        $strTemp         = $this->standardizePath($GLOBALS['SYC_PATH']['tmp']);
+        $strBackupDB   = $this->standardizePath($GLOBALS['SYC_PATH']['db']);
+        $strBackupFile = $this->standardizePath($GLOBALS['SYC_PATH']['file']);
+        $strTemp       = $this->standardizePath($GLOBALS['SYC_PATH']['tmp']);
 
-        $objHt     = new File('system/modules/syncCto/config/.htaccess');
-        $strHT     = $objHt->getContent();
+        $objHt = new File('system/modules/syncCto/config/.htaccess');
+        $strHT = $objHt->getContent();
         $objHt->close();
 
         // Check each one 
@@ -66,13 +146,11 @@ class SyncCtoRunOnce
             new Folder($strTemp);
         }
     }
-    
-    // Helper
 
     /**
      * Standardize path for folder
      * No TL_ROOT, No starting /
-     * 
+     *
      * @return string the normalized path
      */
     public function standardizePath()
@@ -103,6 +181,38 @@ class SyncCtoRunOnce
         }
 
         return preg_replace("/^\//i", "", $strVar);
+    }
+
+    /**
+     * @param string $strValue     Values.
+     *
+     * @param string $strConfigKey Name of the config key.
+     *
+     * @param array  $arrOldValues Old value to be removed.
+     *
+     * @return string Return the cleaned array.
+     */
+    protected function cleanEntries($strValue, $strConfigKey, $arrOldValues)
+    {
+        $arrConfigEntries = $GLOBALS['SYC_CONFIG'][$strConfigKey];
+        $arrList          = deserialize($strValue);
+        $arrSaveList      = array();
+
+        if (is_array($arrSaveList) && count($arrList) > 0)
+        {
+            foreach ($arrList AS $key => $strValue)
+            {
+                if (!in_array($strValue, $arrConfigEntries) && !in_array($strValue, $arrOldValues))
+                {
+                    $arrSaveList[$key] = $strValue;
+                }
+            }
+
+            $arrSaveList = array_keys(array_flip($arrSaveList));
+            return serialize($arrSaveList);
+        }
+
+        return serialize(array());
     }
 
 }
