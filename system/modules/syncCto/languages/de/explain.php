f47a939892cdf2ba924c58e22da61ce72edff83b
Stefan Heimes <stefan_heimes@hotmail.com>
2014-04-03 10:41:19 +0200

    Update the explain translation.

--- system/modules/syncCto/languages/de/explain.php
+++ system/modules/syncCto/languages/de/explain.php
@@ -9,18 +9,10 @@
  * @filesource
  */
 
-$GLOBALS['TL_LANG']['XPL']['apiKey']['0']['0']           = 'ctoCommunication API-Key';
-$GLOBALS['TL_LANG']['XPL']['apiKey']['0']['1']           = 'Der API-Key ist für die Verschlüsselung bei der Synchronisation von zwei Contao-Installationen zuständig.<br /><br />Der Schlüssel wird immer in den allgemeinen Einstellungen des Clients generiert und muss von dort kopiert und in das dafür bereitgestellte Feld auf dem Server eingefügt werden.';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['0']['0'] = 'Platzhalter am Anfang eines Pfades';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['0']['1'] = 'Jeder Eintrag geht vom TL_ROOT aus. Wenn Sie zum Beispiel "assets/css" angeben, wird nur der Ordner "TL_ROOT/assests/css/" ignoriert aber nicht die Ordner die zum Beipsiel unter "system/modules/my_extension/assets/css/" liegen.';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['1']['0'] = 'Allgemein - Pfad Ende';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['1']['1'] = 'Jeder Eintrag wird automatisch am ende mit einem * versehen. Wenn Sie also zum Beispiel "assets/css" eintrage, würder das System neben dem "assets/css" auch den "assets/css3pie" ignorieren. Daher sollten Sie nach jedem Ordner einen "/" angeben.';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['2']['0'] = 'Alle .github auslassen';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['2']['1'] = 'Wenn Sie einen Ordner auslassen wollen, der an jeder stelle auftaucht, können Sie diesen mit Hilfe des "*" Platzhalte eintragen. So würde */.github/ alle github Ordner auslassen. Außer dem in TL_ROOT, dieser müsste mit .github/ direkt angegeben werdem. ';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['3']['0'] = 'Alle Ordner mit Datum';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['3']['1'] = 'Wenn Sie zum Beispiel Ordner mit forlaufenden Nummer nicht übertragen wollen, können Sie dies mit dem "*" Platzhalter machen oder einfach den Ordner ohne Zahlen und "/" am Ende angeben. Beispiel "backup/file_", dieser Eintrag würde alle Ordner im Backup Verzeichnis  ignorieren sofern dieser mit "file_" anfangen.';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['4']['0'] = 'Platzhalter';
-$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['4']['1'] = 'Anbei eine Liste der Platzhalter: <br /> "?"  Ein belibeges Zeichen <br /> "*"  Belibege viele Zeichen';
-$GLOBALS['TL_LANG']['XPL']['security']['0']['0']         = 'Verschlüsselungs-Engine';
-$GLOBALS['TL_LANG']['XPL']['security']['0']['1']         = 'syncCto bietet standardmäßig drei Arten der Synchronisation an. Zwei mit einer Verschlüsselung und eine ohne.<br /><br />Die Synchronisation ohne Verschlüsselung ist nur bei internen Projekten zu empfehlen, da sonst Angriffe von außen nicht auszuschließen sind.';
-
+$GLOBALS['TL_LANG']['XPL']['apiKey']['0']            = array('ctoCommunication API Key', 'Der API Key ist für die Verschlüsselung bei der Synchronisation von zwei Contao-Installationen zuständig.<br /><br />Der Schlüssel wird immer in den allgemeinen Einstellungen des Clients generiert und muss von dort kopiert und in das dafür bereitgestellte Feld auf dem Server eingefügt werden.');
+$GLOBALS['TL_LANG']['XPL']['security']['0']          = array('Verschlüsselungs-Engine', 'syncCto bietet standardmäßig drei Arten der Synchronisation an. Zwei mit einer Verschlüsselung und eine ohne.<br /><br />Die Synchronisation ohne Verschlüsselung ist nur bei internen Projekten zu empfehlen da sonst Angriffe von außen nicht auszuschließen sind.');
+$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['0']  = array('Platzhalter am Anfang eines Pfades', 'Jeder Eintrag geht vom Root der Installation (TL_ROOT) aus. Bei dem Ordner "assets/css" wird der Pfad "TL_ROOT/assets/css/" ignoriert, aber z.B. nicht der Pfad unter "system/modules/example-extension/assets/css/".');
+$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['1']  = array('Platzhalter am Ende eines Pfades', 'Jeder Eintrag wird automatisch am Ende mit einem * versehen. Bei dem Platzhalter "assets/css", würde syncCto auch den Ordner unter "assets/css3pie" ignorieren. Daher sollte nach jedem explizitem Ordner ein "/" angeben werden.');
+$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['2']  = array('Beispiel 1: .idea Ordner ignorieren', 'Ordner können mit dem * Selektor systemweit ignoriert werden. Zum Beispiel werden mit dem Platzhalter */.idea/ alle Ordner mit dem Namen .idea ignoriert.');
+$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['3']  = array('Beispiel 2: Fortlaufende Nummerierung', 'Ordner mit fortlaufender Nummerierung können mit dem * Selektor ignoriert werden. Zum Beispiel Backupverzeichnisse wie "backup/file_" oder "backup/db_" werden dadurch ignoriert, sofern sie mit "file_" oder "db_" anfangen.');
+$GLOBALS['TL_LANG']['XPL']['folder_blacklist']['4']  = array('Platzhalter', 'Derzeit verfügbare Platzhalter: <br /> "?" - Ein beliebiges Zeichen <br /> "*" - Beliebig viele Zeichen');
