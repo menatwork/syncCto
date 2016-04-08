a4d44a88b43ccc48df790342f0294f954d537c51
Stefan Heimes <stefan_heimes@hotmail.com>
2016-04-08 12:15:42 +0200

    Update the ping.js
    - Add check if we got the objects we want.
    - Change the handling to make multiple calls without waiting.
    - Cleanup code.

--- system/modules/syncCto/assets/js/ping.js
+++ system/modules/syncCto/assets/js/ping.js
@@ -1,85 +1,112 @@
 /* Copyright (c) MEN AT WORK 2014 :: LGPL license */
 
-function sendNextRequest(_strToken, _objElements, _intIndex)
+function sendNextRequest(_strToken, _objElement)
 {
-    item = _objElements[_intIndex];
+    var item = _objElement;
+    var element = $(_objElement);
+
+    if (typeof element == "undefined" || element == null)
+    {
+        console.log('Empty item for ping.');
+        return;
+    }
+
+    element = element.getSiblings('span');
+    if (typeof element == "undefined" || element == null)
+    {
+        console.log('Empty sibling for ping.');
+        return;
+    }
+
+    element = element.getChildren('span.client-id');
+    if (typeof element == "undefined" || element == null)
+    {
+        console.log('Empty children for ping.');
+        return;
+    }
+
+    element = element[0];
+    if (typeof element == "undefined" || element == null)
+    {
+        console.log('Empty sub element for ping.');
+        return;
+    }
+
+    element = element.getProperty("text").toString();
+    if (typeof element == "undefined" || element == null)
+    {
+        console.log('Empty string for ping.');
+        return;
+    }
 
     // Get client id
-    var clientID = item.getSiblings('span').getChildren('span.client-id')[0].getProperty("text").toString();
+    var clientID = element;
     var data;
 
     // Check if the request token is set
-    if( typeof(REQUEST_TOKEN) !== 'undefined' )
+    if (typeof(REQUEST_TOKEN) !== 'undefined')
     {
         data = {
-            "isAjax"    : 1,
-            "action"    : "syncCtoPing",
-            "clientID"  : clientID,
-            "REQUEST_TOKEN" : _strToken
+            "isAjax":        1,
+            "action":        "syncCtoPing",
+            "clientID":      clientID,
+            "REQUEST_TOKEN": _strToken
         }
     }
     else
     {
         data = {
-            "isAjax"    : 1,
-            "action"    : "syncCtoPing",
-            "clientID"  : clientID
+            "isAjax":   1,
+            "action":   "syncCtoPing",
+            "clientID": clientID
         }
     }
 
     // Send new request for ping
     new Request.JSON({
-        method:'post',
-        url:window.location.href,
-        data: data,
-        evalScripts:false,
-        evalResponse:false,
-        onSuccess:function(json){ 
-            if(json.success == false)
-            {
-                item.setProperty('src','system/modules/syncCto/assets/images/js/gray.png');
-                item.setProperty('title',json.msg);
-            }
-            else if(json.value == 0)
-            {
-                item.setProperty('src','system/modules/syncCto/assets/images/js/gray.png');
-                item.setProperty('title',json.msg);
-            }
-            else if(json.value == 1)
-            {
-                item.setProperty('src','system/modules/syncCto/assets/images/js/red.png');
-                item.setProperty('title',json.msg);
-            }
-            else if(json.value == 2)
-            {
-                item.setProperty('src','system/modules/syncCto/assets/images/js/blue.png');
-                item.setProperty('title',json.msg);
-            }
-            else if(json.value == 3)
-            {
-                item.setProperty('src','system/modules/syncCto/assets/images/js/orange.png');
-                item.setProperty('title',json.msg);
-            }
-            else if(json.value == 4)
-            {
-                item.setProperty('src','system/modules/syncCto/assets/images/js/green.png');
-                item.setProperty('title',json.msg);
-            }
-            
-            if((_objElements.length - 1 ) > _intIndex)
-            {
-                sendNextRequest(json.token, _objElements, (_intIndex + 1));
-            }
-        }.bind(_objElements).bind(_intIndex),
-        onFailure:function(responseTree,responseElements,response,js){
-            // On error show red point
-            item.setProperty('src','system/modules/syncCto/assets/images/js/offline.png');
-        }.bind(_objElements).bind(_intIndex)
+        method:       'post',
+        url:          window.location.href,
+        data:         data,
+        evalScripts:  false,
+        evalResponse: false,
+        onSuccess:    function (json)
+                      {
+                          if (json.success == false || json.value == 0)
+                          {
+                              item.setProperty('src', 'system/modules/syncCto/assets/images/js/gray.png');
+                              item.setProperty('title', json.msg);
+                          }
+                          else if (json.value == 1)
+                          {
+                              item.setProperty('src', 'system/modules/syncCto/assets/images/js/red.png');
+                              item.setProperty('title', json.msg);
+                          }
+                          else if (json.value == 2)
+                          {
+                              item.setProperty('src', 'system/modules/syncCto/assets/images/js/blue.png');
+                              item.setProperty('title', json.msg);
+                          }
+                          else if (json.value == 3)
+                          {
+                              item.setProperty('src', 'system/modules/syncCto/assets/images/js/orange.png');
+                              item.setProperty('title', json.msg);
+                          }
+                          else if (json.value == 4)
+                          {
+                              item.setProperty('src', 'system/modules/syncCto/assets/images/js/green.png');
+                              item.setProperty('title', json.msg);
+                          }
+                      }.bind(item),
+        onFailure:    function (responseTree, responseElements, response, js)
+                      {
+                          // On error show red point.
+                          item.setProperty('src', 'system/modules/syncCto/assets/images/js/red.png');
+                      }.bind(item)
     }).send();
 }
 
-window.addEvent("domready", function() {
-
+window.addEvent("domready", function ()
+{
     if (typeof Contao !== 'undefined' && typeof Contao.request_token !== 'undefined')
     {
         REQUEST_TOKEN = Contao.request_token;
@@ -92,7 +119,16 @@ window.addEvent("domready", function() {
 
     if ($$('img.ping').length != 0)
     {
-        sendNextRequest(REQUEST_TOKEN, $$('img.ping'), 0);
-    }
+        var elements = $$('img.ping');
+        for (var key in elements)
+        {
+            // Skip loop if the property is from prototype.
+            if (!elements.hasOwnProperty(key) || key == 'length')
+            {
+                continue;
+            }
 
+            sendNextRequest(REQUEST_TOKEN, elements[key]);
+        }
+    }
 });
