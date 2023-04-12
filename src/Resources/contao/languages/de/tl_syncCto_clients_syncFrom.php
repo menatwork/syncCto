b065e8333cf4083260ca1272b3d307748bda5183
e-spin <gitlab@e-spin.de>
2023-04-12 09:10:26 +0200

    Fix table name tl_files

--- src/Resources/contao/languages/de/tl_syncCto_clients_syncFrom.php
+++ src/Resources/contao/languages/de/tl_syncCto_clients_syncFrom.php
@@ -22,7 +22,7 @@ $GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['edit']                      
  */
 $GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['sync_options']                      = array('Dateien synchronisieren', 'Wählen Sie bitte aus, welche Dateien synchronisiert werden sollen.');
 $GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['database_check']                    = array('Datenbank synchronisieren', 'Wählen Sie diese Option, wenn Sie die Datenbank synchronisieren wollen.');
-$GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['tl_files_check']                    = array('\'tl-files\' überschreiben', 'Wählen Sie diese Option, wenn Sie die \'tl-files\' Tabelle synchronisieren wollen.');
+$GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['tl_files_check']                    = array('\'tl_files\' überschreiben', 'Wählen Sie diese Option, wenn Sie die \'tl_files\' Tabelle synchronisieren wollen.');
 $GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['systemoperations_check']            = array('Systemwartung aktivieren', 'Wählen Sie diese Option, wenn die Systemwartung durchgeführt werden soll.');
 $GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['systemoperations_maintenance']      = array('Daten bereinigen', 'Bitte wählen Sie die zu bereinigenden Daten aus.');
 $GLOBALS['TL_LANG']['tl_syncCto_clients_syncFrom']['attention_flag']                    = array('Warnhinweis aktivieren', 'Wählen Sie diese Option, wenn der Warnhinweis auf dem Client aktiviert werden soll.');
