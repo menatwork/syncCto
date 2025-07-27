60ec8735bb24527f47d9ceea5c2190bce95717f6
Stefan Heimes <stefan_heimes@hotmail.com>
2025-07-27 22:02:49 +0200

    Some changes for the popup
    - Add a new basic class
    - Move some parts from other popups to the basic one
    - Correct JS und CSS files

--- src/Controller/APopUpController.php
+++ src/Controller/APopUpController.php
@@ -3,23 +3,70 @@
 namespace MenAtWork\SyncCto\Controller;
 
 use Contao\Backend;
+use Contao\BackendPopup;
+use Contao\BackendTemplate;
 use Contao\BackendUser;
+use Contao\Config;
+use Contao\CoreBundle\Exception\AccessDeniedException;
+use Contao\CoreBundle\Image\Preview\MissingPreviewProviderException;
+use Contao\CoreBundle\Image\Preview\UnableToGeneratePreviewException;
+use Contao\Database;
+use Contao\Date;
+use Contao\Dbafs;
+use Contao\Environment;
+use Contao\File;
+use Contao\FilesModel;
+use Contao\Folder;
+use Contao\Image\PictureConfiguration;
+use Contao\Image\PictureConfigurationItem;
+use Contao\Image\ResizeConfiguration;
 use Contao\Input;
+use Contao\StringUtil;
 use Contao\System;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-abstract class APopUpController
+abstract class APopUpController extends Backend
 {
+    /**
+     * The title of the popup.
+     *
+     * @var string
+     */
+    protected string $title = 'SyncCto';
 
-    public function initSystem()
-    {
-        System::getContainer()->get('contao.framework')->initialize();
-        System::loadLanguageFile('default');
-    }
+    /**
+     * The template to use.
+     *
+     * @var string
+     */
+    protected string $template = 'be_syncCto_popup';
 
-    public function setupTemplate()
+    /**
+     * The session.
+     *
+     * @var SessionInterface
+     */
+    protected SessionInterface $session;
+
+    /**
+     * The template object.
+     *
+     * @var BackendTemplate
+     */
+    protected BackendTemplate $baseTemplate;
+
+    /**
+     * APopUpController constructor.
+     */
+    public function __construct()
     {
-        // Setup.
-        \define('TL_ASSETS_URL', '');
+        parent::__construct();
+
+        if (!System::getContainer()->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException('Access denied');
+        }
 
         // Set language from get or user
         if (Input::get('language') != '') {
@@ -27,20 +74,11 @@ abstract class APopUpController
         } else {
             $GLOBALS['TL_LANGUAGE'] = BackendUser::getInstance()->language;
         }
+        System::loadLanguageFile('default');
 
-        // Clear all we want a clear array for this windows.
-        $GLOBALS['TL_CSS'] = [];
-        $GLOBALS['TL_JAVASCRIPT'] = [];
-
-        // Set stylesheets.
-        $GLOBALS['TL_CSS'][] = 'system/themes/' . Backend::getTheme() . '/basic.css';
-        $GLOBALS['TL_CSS'][] = 'bundles/synccto/css/compare.css';
-
-        // Set javascript.
-        $GLOBALS['TL_JAVASCRIPT'][] = 'assets/mootools/js/mootools-core.min.js';
-        $GLOBALS['TL_JAVASCRIPT'][] = 'assets/mootools/js/mootools-more.min.js';
-        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/synccto/js/compare.js';
-        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/synccto/js/htmltable.js';
+        /** @var RequestStack $requestStack */
+        $requestStack = System::getContainer()->get('request_stack');
+        $this->session = $requestStack->getSession();
     }
 
     /**
@@ -54,5 +92,54 @@ abstract class APopUpController
                      ->get('contao.csrf.token_manager')
                      ->getDefaultTokenValue()
         ;
+    }
+
+    /**
+     * Do the action.
+     */
+    abstract protected function doAction(): string;
+
+    /**
+     * Run the controller and parse the template
+     *
+     * @return Response
+     */
+    public function runAction(): Response
+    {
+        $container = System::getContainer();
+        $this->baseTemplate = new BackendTemplate($this->template);
+
+        // Clear all we want a clear array for this windows.
+        if (empty($GLOBALS['TL_CSS'])) {
+            $GLOBALS['TL_CSS'] = [];
+        }
+        if (empty($GLOBALS['TL_JAVASCRIPT'])) {
+            $GLOBALS['TL_JAVASCRIPT'] = [];
+        }
+
+        $this->baseTemplate->content = $this->doAction();
+
+        // Set stylesheets.
+        $GLOBALS['TL_CSS'][] = 'bundles/synccto/css/compare.css';
+
+        // Set javascript.
+        $GLOBALS['TL_JAVASCRIPT'][] = 'assets/mootools/js/mootools-core.min.js';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'assets/mootools/js/mootools-more.min.js';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/synccto/js/compare.js';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/synccto/js/htmltable.js';
+
+        $this->baseTemplate->theme = Backend::getTheme();
+        $this->baseTemplate->base = Environment::get('base');
+        $this->baseTemplate->language = $GLOBALS['TL_LANGUAGE'];
+        $this->baseTemplate->title = StringUtil::specialchars(
+            ($this->title ?? $GLOBALS['TL_CONFIG']['websiteTitle'])
+        );
+        $this->baseTemplate->headline = basename($this->strFile ?? '');
+        $this->baseTemplate->host = Backend::getDecodedHostname();
+        $this->baseTemplate->charset = $container->getParameter('kernel.charset');
+        $this->baseTemplate->labels = (object) $GLOBALS['TL_LANG']['MSC'];
+        $this->baseTemplate->requestToken = $this->getRequestToken();;
+
+        return $this->baseTemplate->getResponse();
     }
 }
